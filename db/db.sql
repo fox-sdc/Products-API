9b594914841426c5a028ea1c59e30944347837c0
Sean Trinh <sean.a.trinh@gmail.com>
2022-04-20 14:37:57 -0700

    Update .gitignore, db.sql, and created /etl/local_load.sql
    - .gitignore updated to include .DS_Store
    - db.sql updates: single DROP statement at top of script, updated schema data types and field names to match .csvs
    - /etl/local_load.sql created: locally importing CSV data into psql as well as including ALTER TABLE statements to rename column names to match desired output

--- db/db.sql
+++ db/db.sql
@@ -1,25 +1,18 @@
 -- ---
--- Globals
--- ---
-
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
-
--- ---
--- Table 'products'
+-- Table "products"
 --
 -- ---
 
-DROP TABLE IF EXISTS `products`;
+DROP TABLE IF EXISTS "products", "styles", "photos", "skus", "features", "related_products";
 
-CREATE TABLE `products` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `slogan` VARCHAR(255) NULL DEFAULT NULL,
-  `description` VARCHAR(255) NULL DEFAULT NULL,
-  `category` VARCHAR(255) NULL DEFAULT NULL,
-  `default_price` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "products" (
+  "id" INTEGER NULL DEFAULT NULL,
+  "name" VARCHAR(125) NULL DEFAULT NULL,
+  "slogan" VARCHAR(255) NULL DEFAULT NULL,
+  "description" VARCHAR(5000) NULL DEFAULT NULL,
+  "category" VARCHAR(125) NULL DEFAULT NULL,
+  "default_price" VARCHAR(125) NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
@@ -27,16 +20,14 @@ CREATE TABLE `products` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `styles`;
-
-CREATE TABLE `styles` (
-  `product_id` INTEGER NULL DEFAULT NULL,
-  `style_id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `original_price` VARCHAR(255) NULL DEFAULT NULL,
-  `sale_price` VARCHAR(255) NULL DEFAULT NULL,
-  `default?` VARCHAR(255) NOT NULL DEFAULT 'NULL',
-  PRIMARY KEY (`style_id`)
+CREATE TABLE "styles" (
+  "id" INTEGER NULL DEFAULT NULL,
+  "productId" INTEGER NULL DEFAULT NULL,
+  "name" VARCHAR(125) NULL DEFAULT NULL,
+  "sale_price" VARCHAR(125) NULL DEFAULT NULL,
+  "original_price" VARCHAR(125) NULL DEFAULT NULL,
+  "default_style" BOOLEAN,
+  PRIMARY KEY ("id")
 );
 
 -- ---
@@ -44,14 +35,12 @@ CREATE TABLE `styles` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `photos`;
-
-CREATE TABLE `photos` (
-  `style_id` INTEGER NULL DEFAULT NULL,
-  `photo_id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `thumbnail_url` VARCHAR(1000) NULL DEFAULT NULL,
-  `url` VARCHAR(1000) NULL DEFAULT NULL,
-  PRIMARY KEY (`photo_id`)
+CREATE TABLE "photos" (
+  "id" INTEGER NULL DEFAULT NULL,
+  "styleId" INTEGER NULL DEFAULT NULL,
+  "url" VARCHAR(5000) NULL DEFAULT NULL,
+  "thumbnail_url" VARCHAR(40000) NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
@@ -59,14 +48,12 @@ CREATE TABLE `photos` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `skus`;
-
-CREATE TABLE `skus` (
-  `style_id` INTEGER NULL DEFAULT NULL,
-  `sku_id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `quantity` INTEGER NULL DEFAULT NULL,
-  `size` CHAR(10) NULL DEFAULT NULL,
-  PRIMARY KEY (`sku_id`)
+CREATE TABLE "skus" (
+  "id" INTEGER NULL DEFAULT NULL,
+  "styleId" INTEGER NULL DEFAULT NULL,
+  "size" CHAR(10) NULL DEFAULT NULL,
+  "quantity" INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
@@ -74,63 +61,60 @@ CREATE TABLE `skus` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `features`;
-
-CREATE TABLE `features` (
-  `product_id` INTEGER NULL DEFAULT NULL,
-  `feature_id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `feature` VARCHAR(255) NULL DEFAULT NULL,
-  `value` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`feature_id`)
+CREATE TABLE "features" (
+  "id" INTEGER NULL DEFAULT NULL,
+  "product_id" INTEGER NULL DEFAULT NULL,
+  "feature" VARCHAR(125) NULL DEFAULT NULL,
+  "value" VARCHAR(125) NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
--- Table 'realted_products'
+-- Table 'related_products'
 --
 -- ---
 
-DROP TABLE IF EXISTS `realted_products`;
-
-CREATE TABLE `realted_products` (
-  `product_id` INTEGER NULL DEFAULT NULL,
-  `related_product_id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  PRIMARY KEY (`related_product_id`)
+CREATE TABLE "related_products" (
+  "id" INTEGER NULL DEFAULT NULL,
+  "current_product_id" INTEGER NULL DEFAULT NULL,
+  "related_product_id" INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
 -- Foreign Keys
 -- ---
 
-ALTER TABLE `styles` ADD FOREIGN KEY (product_id) REFERENCES `products` (`id`);
-ALTER TABLE `photos` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`style_id`);
-ALTER TABLE `skus` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`style_id`);
-ALTER TABLE `features` ADD FOREIGN KEY (product_id) REFERENCES `products` (`id`);
-ALTER TABLE `realted_products` ADD FOREIGN KEY (product_id) REFERENCES `products` (`id`);
+ALTER TABLE "styles" ADD FOREIGN KEY ("productId") REFERENCES "products" ("id");
+ALTER TABLE "photos" ADD FOREIGN KEY ("styleId") REFERENCES "styles" ("id");
+ALTER TABLE "skus" ADD FOREIGN KEY ("styleId") REFERENCES "styles" ("id");
+ALTER TABLE "features" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+ALTER TABLE "related_products" ADD FOREIGN KEY ("current_product_id") REFERENCES "products" ("id");
 
 -- ---
 -- Table Properties
 -- ---
 
--- ALTER TABLE `products` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `styles` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `photos` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `skus` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `features` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `realted_products` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "products" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "styles" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "photos" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "skus" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "features" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "related_products" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `products` (`id`,`name`,`slogan`,`description`,`category`,`default_price`) VALUES
+-- INSERT INTO "products" ("id","name","slogan","description","category","default_price") VALUES
 -- ('','','','','','');
--- INSERT INTO `styles` (`product_id`,`style_id`,`name`,`original_price`,`sale_price`,`default?`) VALUES
+-- INSERT INTO "styles" ("id","productId","name","sale_price","original_price","default_style") VALUES
 -- ('','','','','','');
--- INSERT INTO `photos` (`style_id`,`photo_id`,`thumbnail_url`,`url`) VALUES
+-- INSERT INTO "photos" ("id","styleId","url","thumbnail_url") VALUES
 -- ('','','','');
--- INSERT INTO `skus` (`style_id`,`sku_id`,`quantity`,`size`) VALUES
+-- INSERT INTO "skus" ("id","styleId","size","quantity") VALUES
 -- ('','','','');
--- INSERT INTO `features` (`product_id`,`feature_id`,`feature`,`value`) VALUES
+-- INSERT INTO "features" ("id","product_id","feature","value") VALUES
 -- ('','','','');
--- INSERT INTO `realted_products` (`product_id`,`related_product_id`) VALUES
--- ('','');
+-- INSERT INTO "related_products" ("id","current_product_id","related_product_id") VALUES
+-- ('','','');
